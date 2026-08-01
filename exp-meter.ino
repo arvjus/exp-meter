3fb59dccc40759b0804742cbe93d9ed97cef3bb4
Arvid Juskaitis <arvydas.juskaitis@gmail.com>
2026-08-01 18:13:08 +0200

    implement zone method

--- exp-meter.ino
+++ exp-meter.ino
@@ -31,12 +31,12 @@ LiquidCrystal lcd(12, 11, 10, 7, 6, 5, 4);
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
 int8_t mode = -1;
-float lux, calibration_lux = 0, ev, ev_min, ev_min_total, ev_max, ev_max_total, calibration_ev = 0;
-int8_t ev_min_count = 0, ev_max_count = 0;
+float lux, lux_calibration = 0, ev, ev_highlights, ev_highlights_total, ev_shadows, ev_shadows_total, ev_calibration = 0;
+int8_t ev_highlights_count = 0, ev_shadows_count = 0;
 unsigned long last_released = 0l;
-volatile int8_t submode = SUBMODE_EXPOSURE;
-volatile float brghtn_comp = 0, cntrst_comp = 0;
-
+volatile int8_t method = -1, submode = SUBMODE_EXPOSURE;
+volatile float comp_brightness = 0, comp_contrast = 0;
+volatile int8_t zone_highlights = 8, zone_shadows = 1;
 
 void setup()
 {
@@ -71,6 +71,13 @@ void loop()
     lcd.clear();
     delay(200);
   }
+
+  int8_t tmp_method = digitalRead(METHOD_PIN);
+  if (tmp_method != method) {
+    method = tmp_method;
+    lcd.clear();
+    delay(200);
+  }
   
   lux = getLuminosity();
   ev = logf((float)lux / 2.5f) * INV_LOG2;
@@ -92,7 +99,6 @@ void loop()
       delay(200);
     }
 
-    int8_t method = digitalRead(METHOD_PIN);
     int8_t measure_flag = false, reset_flag = false;
 
     // Check measure button
@@ -113,57 +119,74 @@ void loop()
       if (reset_flag) 
         reset();
       else if (measure_flag) {
-        ev_min = ev_min == 0.0 ? ev : min(ev, ev_min);
-        ev_max = max(ev, ev_max);
+        ev_highlights = ev_highlights == 0.0 ? ev : min(ev, ev_highlights);
+        ev_shadows = max(ev, ev_shadows);
       }
       
-      if (ev_min && ev_min < ev_max) {
+      const float ev_contrast = ev_shadows - ev_highlights;
+      if (ev_highlights && ev_contrast > 0.0f) {
         int8_t paper = digitalRead(PAPER_PIN);
-
-        filter = ((paper == PAPER_RC5) ? calculate_filter_rc5(ev_max - ev_min) : calculate_filter_fb(ev_max - ev_min)) + cntrst_comp;
-        exposure = (paper == PAPER_RC5) ? calculate_exposure_rc5(filter, ev_min) : calculate_exposure_fb(filter, ev_min);
-        exposure *= pow(2, -brghtn_comp);
+        if (method == METHOD_ZONE && zone_highlights - zone_shadows >= 1) {
+          const float zone_step = ev_contrast / (zone_highlights - zone_shadows);
+          const uint8_t target_zone_shadows = 1, target_zone_highlights = 8;
+          const float target_ev_highlights = ev_highlights - (target_zone_highlights - zone_highlights) * zone_step;
+          const float target_ev_shadows = ev_shadows + (zone_shadows - target_zone_shadows) * zone_step;
+          filter = ((paper == PAPER_RC5) ? calculate_filter_rc5(target_ev_shadows - target_ev_highlights) : calculate_filter_fb(target_ev_shadows - target_ev_highlights));
+          exposure = (paper == PAPER_RC5) ? calculate_exposure_rc5(filter, target_ev_highlights) : calculate_exposure_fb(filter, target_ev_highlights);
+        } else if (method == METHOD_COMP) {
+          filter = ((paper == PAPER_RC5) ? calculate_filter_rc5(ev_contrast) : calculate_filter_fb(ev_contrast)) + comp_contrast;
+          exposure = (paper == PAPER_RC5) ? calculate_exposure_rc5(filter, ev_highlights) : calculate_exposure_fb(filter, ev_highlights);
+          exposure *= pow(2, -comp_brightness);
+        }
       }
 
-      sprintf(buffer, "E%5s%5s%5s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_min, 5, 2, buffer3), dtostrf(ev_max, 5, 2, buffer4));
+      sprintf(buffer, "E%5s%5s%5s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_highlights, 5, 2, buffer3), dtostrf(ev_shadows, 5, 2, buffer4));
       lcd.setCursor(0, 0);
       lcd.print(buffer);
 
-      sprintf(buffer, "%6s %3ds F%3s ", dtostrf(ev_max - ev_min, 6, 2, buffer2), exposure, dtostrf(filter, 3, 1, buffer3));
+      sprintf(buffer, "%6s %3ds F%3s ", dtostrf(ev_contrast, 6, 2, buffer2), exposure, dtostrf(filter, 3, 1, buffer3));
       lcd.setCursor(0, 1);
       lcd.print(buffer);
 
     /* Measure highlights */
     } else if (submode == SUBMODE_HIGHLIGHTS) {
         if (reset_flag) {
-          ev_min = 0.0;
-          ev_min_total = 0.0;
-          ev_min_count = 0;
-          brghtn_comp = 0.0;
+          ev_highlights = 0.0;
+          ev_highlights_total = 0.0;
+          ev_highlights_count = 0;
+          comp_brightness = 0.0;
+          zone_highlights = 8;
         } else if (measure_flag) {
-          ev_min_total += ev;
-          ev_min_count ++;
-          ev_min = ev_min_total / ev_min_count;
+          ev_highlights_total += ev;
+          ev_highlights_count ++;
+          ev_highlights = ev_highlights_total / ev_highlights_count;
         }
 
-        sprintf(buffer, "H%5s%5s %4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_min, 5, 2, buffer3), dtostrf(brghtn_comp, 4, 1, buffer4));
+        if (method == METHOD_ZONE) 
+          sprintf(buffer, "H%5s%5s %-4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_highlights, 5, 2, buffer3), display_zone(zone_highlights, buffer4));
+        else if (method == METHOD_COMP) 
+          sprintf(buffer, "H%5s%5s %4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_highlights, 5, 2, buffer3), dtostrf(comp_brightness, 4, 1, buffer4));
         lcd.setCursor(0, 0);
         lcd.print(buffer);
         
     /* Measure shadows */
     } else if (submode == SUBMODE_SHADOWS) {
         if (reset_flag) {
-          ev_max = 0.0;
-          ev_max_total = 0.0;
-          ev_max_count = 0;
-          cntrst_comp = 0.0;
+          ev_shadows = 0.0;
+          ev_shadows_total = 0.0;
+          ev_shadows_count = 0;
+          comp_contrast = 0.0;
+          zone_shadows = 1;
         } else if (measure_flag) {
-          ev_max_total += ev;
-          ev_max_count ++;
-          ev_max = ev_max_total / ev_max_count;
+          ev_shadows_total += ev;
+          ev_shadows_count ++;
+          ev_shadows = ev_shadows_total / ev_shadows_count;
         }
 
-        sprintf(buffer, "S%5s%5s %4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_max, 5, 2, buffer3), dtostrf(cntrst_comp, 4, 1, buffer4));
+        if (method == METHOD_ZONE) 
+          sprintf(buffer, "S%5s%5s %-4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_shadows, 5, 2, buffer3), display_zone(zone_shadows, buffer4));
+        else if (method == METHOD_COMP) 
+          sprintf(buffer, "S%5s%5s %4s", dtostrf(ev, 5, 2, buffer2), dtostrf(ev_shadows, 5, 2, buffer3), dtostrf(comp_contrast, 4, 1, buffer4));
         lcd.setCursor(0, 0);
         lcd.print(buffer);
     }
@@ -176,12 +199,12 @@ void loop()
   } else if (mode == MODE_DENSITY) {
     // calibration
     if (!digitalRead(MEASURE_PIN)) {
-      calibration_lux = lux;
-      calibration_ev = ev;
+      lux_calibration = lux;
+      ev_calibration = ev;
     }
 
-    float d = log(calibration_lux / lux) / log(10);
-    sprintf(buffer, "D%6s %5s", dtostrf(d, 6, 3, buffer2), dtostrf(calibration_ev - ev, 5, 2, buffer3));
+    float d = log(lux_calibration / lux) / log(10);
+    sprintf(buffer, "D%6s %5s", dtostrf(d, 6, 3, buffer2), dtostrf(ev_calibration - ev, 5, 2, buffer3));
     lcd.setCursor(0, 0);
     lcd.print(buffer);
   }
@@ -191,14 +214,16 @@ void reset()
 {
   lux = 0;
   ev = 0.0;
-  ev_min = 0.0;
-  ev_min_total = 0.0;
-  ev_min_count = 0;
-  ev_max = 0.0;
-  ev_max_total = 0.0;
-  ev_max_count = 0;
-  brghtn_comp = 0.0;
-  cntrst_comp = 0.0;
+  ev_highlights = 0.0;
+  ev_highlights_total = 0.0;
+  ev_highlights_count = 0;
+  ev_shadows = 0.0;
+  ev_shadows_total = 0.0;
+  ev_shadows_count = 0;
+  comp_brightness = 0.0;
+  comp_contrast = 0.0;
+  zone_highlights = 8;
+  zone_shadows = 1;
   last_released = 0l;
 
   lcd.clear();
@@ -206,28 +231,28 @@ void reset()
 
 float getLuminosity()
 {
-    uint16_t broadband, infrared;
+  uint16_t broadband, infrared;
 
-    // Fast measurement
-    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);
-    delay(110);
-    tsl.getLuminosity(&broadband, &infrared);
+  // Fast measurement
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);
+  delay(110);
+  tsl.getLuminosity(&broadband, &infrared);
 
-    if (broadband >= 8)
-        return (float)broadband;
+  if (broadband >= 8)
+      return (float)broadband;
 
-    // Low light: longer integration
-    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);
-    delay(420);
-    tsl.getLuminosity(&broadband, &infrared);
+  // Low light: longer integration
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);
+  delay(420);
+  tsl.getLuminosity(&broadband, &infrared);
 
-    return broadband / 4.0f;
+  return broadband / 4.0f;
 }
 
 void readEncoder() {
   static unsigned long last_interrupt = 0;
   unsigned long now = millis();
-  if (now - last_interrupt < 50)
+  if (now - last_interrupt < 100)
     return;
 
   // PIND & 0b00000100 isolates Pin 2 (Bit 2)
@@ -235,21 +260,54 @@ void readEncoder() {
   bool clkState = PIND & _BV(PD2); 
   bool dtState  = PIND & _BV(PD3);
 
-  if (submode == SUBMODE_HIGHLIGHTS) {
-    if (clkState != dtState && brghtn_comp < 1.5f) {
-      brghtn_comp += 0.5f;
-    } else if (clkState == dtState && brghtn_comp > -1.5f) {
-      brghtn_comp -= 0.5f;
+  if (method == METHOD_ZONE) {
+    if (submode == SUBMODE_HIGHLIGHTS) {
+      if (clkState != dtState && zone_highlights < 9) {
+        zone_highlights ++;
+      } else if (clkState == dtState && zone_highlights > 5) {
+        zone_highlights --;
+      }
+    } else if (submode == SUBMODE_SHADOWS) {
+      if (clkState != dtState && zone_shadows < 5) {
+        zone_shadows ++;
+      } else if (clkState == dtState && zone_shadows > 1) {
+        zone_shadows --;
+      }
     }
-  } else if (submode == SUBMODE_SHADOWS) {
-    if (clkState != dtState && cntrst_comp < 1.5f) {
-      cntrst_comp += 0.5f;
-    } else if (clkState == dtState && cntrst_comp > -1.5f) {
-      cntrst_comp -= 0.5f;
+  } else if (method == METHOD_COMP) {
+    if (submode == SUBMODE_HIGHLIGHTS) {
+      if (clkState != dtState && comp_brightness < 2.0f) {
+        comp_brightness += 0.5f;
+      } else if (clkState == dtState && comp_brightness > -2.0f) {
+        comp_brightness -= 0.5f;
+      }
+    } else if (submode == SUBMODE_SHADOWS) {
+      if (clkState != dtState && comp_contrast < 2.0f) {
+        comp_contrast += 0.5f;
+      } else if (clkState == dtState && comp_contrast > -2.0f) {
+        comp_contrast -= 0.5f;
+      }
     }
   }
   
   last_interrupt = now;
+}
+
+char* display_zone(uint8_t zone, char* buffer) {
+  switch (zone) {
+  case 1: strcpy(buffer, "I"); break;
+  case 2: strcpy(buffer, "II"); break;
+  case 3: strcpy(buffer, "III"); break;
+  case 4: strcpy(buffer, "IV"); break;
+  case 5: strcpy(buffer, "V"); break;
+  case 6: strcpy(buffer, "VI"); break;
+  case 7: strcpy(buffer, "VII"); break;
+  case 8: strcpy(buffer, "VIII"); break;
+  case 9: strcpy(buffer, "IX"); break;
+  default: strcpy(buffer, "");
+  }
+
+  return buffer;
 }
 
 // To calculate filter, we use quadratic function:
@@ -258,7 +316,7 @@ void readEncoder() {
 // filter = (c * ev + b) * ev + a;
 
 // 0.152528045  -2.695569866  8.743406703
-float calculate_filter_rc5(float ev)
+float calculate_filter_rc5(const float ev)
 {
   float filter = (0.152528045f * ev + -2.695569866f) * ev + 8.743406703f;
 
@@ -271,7 +329,7 @@ float calculate_filter_rc5(float ev)
 }
 
 // 0.082128023  -2.105486282  7.656978866
-float calculate_filter_fb(float ev)
+float calculate_filter_fb(const float ev)
 {
     float filter = (0.082128023f * ev + -2.105486282f) * ev + 7.656978866f;
 
@@ -286,7 +344,7 @@ float calculate_filter_fb(float ev)
 // To clalculate exposure, 
 // 3.07 is the EV value used in calibration for getting these exp times- measuring without any filter
 
-uint16_t calculate_exposure_rc5(float filter, float ev_min)
+uint16_t calculate_exposure_rc5(const float filter, const float ev_highlights)
 {
   static float exp_times[] = { 
     2.3101f, 2.3500f,   // 0
@@ -295,10 +353,10 @@ uint16_t calculate_exposure_rc5(float filter, float ev_min)
     2.8678f, 2.9474f,   // 3
     5.5762f, 5.8152f,   // 4
     6.0542f };          // 5
-  return round(exp_times[int(filter * 2)] * pow(2, (3.07 - ev_min)));
+  return round(exp_times[int(filter * 2)] * pow(2, (3.07 - ev_highlights)));
 }
 
-uint16_t calculate_exposure_fb(float filter, float ev_min)
+uint16_t calculate_exposure_fb(const float filter, const float ev_highlights)
 {
   static float exp_times[] = { 
     2.3898f, 2.3898f,   // 0
@@ -307,5 +365,5 @@ uint16_t calculate_exposure_fb(float filter, float ev_min)
     2.7084f, 2.8678f,   // 3
     4.7796f, 4.9588f,   // 4
     5.1779f };          // 5
-  return round(exp_times[int(filter * 2)] * pow(2, (3.07 - ev_min)));
+  return round(exp_times[int(filter * 2)] * pow(2, (3.07 - ev_highlights)));
 }
